fda10258fc67ee2db88d95c751162153f28d366d
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2024-05-26 20:54:13 +0300

    v1.4

---
+++ assets/js/resize.js
@@ -0,0 +1,18 @@
+const blocks = document.querySelectorAll(".project-block");
+
+function updateBlockStyle() {
+    const windowWidth = window.innerWidth;
+    if (windowWidth <= 768) {
+        for (let block of blocks) {
+            console.log(block.classList)
+            block.classList.add("project-block-small");
+        }
+    }   else {
+        for (let block of blocks) {
+            block.classList.remove("project-block-small");
+        }
+    }
+}
+
+window.addEventListener("resize", updateBlockStyle);
+
