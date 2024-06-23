0659a1bc7aa20298be20f19d4e1a49601eee64e7
mikhailVoznyuk <mikhailv.commerce@gmail.com>
2024-06-23 13:25:28 +0300

    v0.66-need-to-fix

--- assets/js/modal_adaptive.js
+++ assets/js/modal_adaptive.js
@@ -7,6 +7,7 @@ var isModalOpened = false;
 
 
 function removeContent() {
+    console.log('раз')
     if (isModalOpened == true) {
         console.log('1')
         for (let content of contents) {
@@ -19,14 +20,15 @@ function removeContent() {
         isModalOpened = false;
     } else {
         console.log(contents);
-        for (let content of contents) {
-            content.classList.add("hidden");
-        }
         for (let image of images) {
             image.removeEventListener("click", removeContent);
         }
-  
+        for (let content of contents) {
+            content.classList.add("hidden");
+        }
+      
         body.addEventListener("click", removeContent);
+        isModalOpened = true;
         
     }
 }
@@ -34,4 +36,5 @@ function removeContent() {
 for (let image of images) {
     image.addEventListener("click", removeContent);
 }
+
 
